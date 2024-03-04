e1fd88c5efe134b65b8ea9749fc4f7c3cc81be37
mrthinh307 <23020708@vnu.edu.vn>
2024-03-04 18:34:43 +0700

    test.cpp

--- test.cpp
+++ test.cpp
@@ -1,7 +1,22 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <algorithm>
+#include <cstring>
 using namespace std;
 
-int main(){
-    cout << "Hello World!" << endl;
+void rFilter(char *s){
+    int l = 0;
+    while(l < strlen(s)){
+        if(!((s[l] >= 'a' && s[l] <= 'z') || (s[l] >= 'A' && s[l] <= 'Z'))){
+            s[l] = '_';
+        }
+        l++;
+    }
+}
+
+int main() {
+    char str[] = "Hello4234.[.]";
+    rFilter(str);
+    std::cout << str << std::endl; 
+
     return 0;
 }
