67239e8d72fcccdbe2b7ee513da116ae14bdc884
mrthinh307 <23020708@vnu.edu.vn>
2024-03-05 21:18:15 +0700

    SDL/BasicDrawFunction.cpp

--- SDL/BasicDrawFunction.cpp
+++ SDL/BasicDrawFunction.cpp
@@ -4,6 +4,18 @@ int SDL_RenderClear(SDL_Renderer* renderer)
 /* ĐẶT MÀU VẼ */ 
 int SDL_SetRenderDrawColor(SDL_Renderer* renderer, Uint8 r, Uint 8 g, Uint8 b, Uint8 a);
 
+ const SDL_Color CYAN_COLOR = {0, 255, 255};
+ const SDL_Color BLUE_COLOR = {0, 0, 255};
+ const SDL_Color ORANGE_COLOR = {255, 165, 0};
+ const SDL_Color YELLOW_COLOR = {255, 255, 0};
+ const SDL_Color LIME_COLOR = {0, 255, 0};
+ const SDL_Color PURPLE_COLOR = {128, 0, 128};
+ const SDL_Color RED_COLOR = {255, 0, 0};
+ const SDL_Color WHITE_COLOR = {255, 255, 255};
+ const SDL_Color BLACK_COLOR = {0, 0, 0};
+ const SDL_Color GREEN_COLOR = {0, 128, 0};
+ const SDL_Color DEFAULT_COLOR = BLACK_COLOR;
+ 
 /* VẼ ĐIỂM */
 int SDL_RenderDrawPoint(SDL_Renderer* renderer, int x, int y);
 
